7fcfce71878fb0694c1401f33776a9696f1f97e2
Daehoon Song <coco.in.bluemoon@gmail.com>
2020-12-04 23:26:22 +0900

    solve: CLASS 3 리모콘 TIME LIMIT SOLVED

--- CLASS 3/1107: 리모콘/solution.py
+++ CLASS 3/1107: 리모콘/solution.py
@@ -1,34 +1,43 @@
+def is_dialable(number, broken):
+    for n in str(number):
+        if n in broken:
+            return False
+    return True
+
+
 def solution(target, broken):
     START = 100
-    broken = set(broken)
+    broken = set(str(broken))
 
-    if target == START:
-        return 0
+    difference = abs(target - START)
+    answer = difference
 
-    target_bigger = target
-    target_set = set(map(int, str(target_bigger)))
-    while len(broken & target_set):
-        target_bigger += 1
-        target_set = set(map(int, str(target_bigger)))
-    counter_bigger = len(str(target_bigger)) + abs(target_bigger - target)
+    for delta in range(difference):
+        target_bigger = target + delta
+        target_smaller = target - delta
 
-    target_smaller = target
-    target_set = set(map(int, str(target_smaller)))
-    while len(broken & target_set):
-        target_smaller -= 1
-        target_set = set(map(int, str(target_smaller)))
-    counter_smaller = len(str(target_smaller)) + abs(target - target_smaller)
+        if target_smaller >= 0 and is_dialable(target_smaller, broken):
+            counter = len(str(target_smaller)) + delta
+            answer = min(answer, counter)
+            break
+        elif is_dialable(target_bigger, broken):
+            counter = len(str(target_bigger)) + delta
+            answer = min(answer, counter)
+            break
 
-    return min(counter_smaller, counter_bigger)
+    return answer
 
 
 if __name__ == "__main__":
     assert solution(5457, [6, 7, 8]) == 6
     assert solution(100, [0, 1, 2, 3, 4]) == 0
     assert solution(500000, [0, 2, 3, 4, 6, 7, 8, 9]) == 11117
+    assert solution(0, [0, 1, 2]) == 4
 
     target = int(input())
     n = int(input())
-    broken = list(map(int, input().split()))
+    broken = list()
+    if n:
+        broken = list(map(int, input().split()))
     answer = solution(target, broken)
     print(answer)
